bb5140d7fbd2b27588c17d86226a0d29c327b558
henrysmile2071 <henrychen.chc@gmail.com>
2025-02-16 15:50:57 +0800

    feat: add postgres & sequelize & models

---
+++ src/models/post.model.js
@@ -0,0 +1,50 @@
+/**
+ * Model class for "post"
+ *
+ * @author Henry Chen
+ *
+ * @param {Sequelize} sequelize - sequelize object
+ * @param {Sequelize.DataTypes} DataTypes - sequelize datatypes
+ *
+ * @returns Post - sequelize model
+ */
+export default (sequelize, DataTypes) => {
+  const Post = sequelize.define('Post', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  }, {
+    tableName: 'post',
+    underscored: true,
+    name: {
+      singular: 'post',
+      plural: 'posts',
+    },
+  });
+
+  Post.associate = models => {
+    models.Post.belongsTo(models.User, { foreignKey: 'userId', targetId: 'id' });
+  };
+  return Post;
+};
---
+++ src/models/user.model.js
@@ -0,0 +1,33 @@
+/**
+ * Model class for "User"
+ *
+ * @author Henry Chen
+ *
+ * @param {Sequelize} sequelize - sequelize object
+ * @param {Sequelize.DataTypes} DataTypes - sequelize datatypes
+ *
+ * @returns User - sequelize model for user entity
+ */
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  }, {
+    tableName: 'user',
+    underscored: true,
+  });
+
+  User.associate = models => {
+    models.User.hasMany(models.Posts, { foreignKey: 'userId', targetId: 'id' });
+  };
+  return User;
+};
